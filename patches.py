4c859091f842c2734e17f8e77df39e9c7eeb4279
Pranav Karawale <pdkarawale31@gmail.com>
2021-08-10 15:20:50 +0530

    feat: print error if input is needed but input file is empty

--- patches.py
+++ patches.py
@@ -1,6 +1,18 @@
-__original_input_fn = input
-def input(arg):
-    result = __original_input_fn(arg)
-    print(result)
-    return result
+import sys
 
+__original_input_fn = input
+
+
+def input(arg):
+    try:
+        result = __original_input_fn(arg)
+        print(result)
+        return result
+    except EOFError:
+        print(
+            "The program is trying to accept input",
+            "but the input file is empty.",
+            "Please add the inputs in the respective file.",
+            file=sys.stderr,
+        )
+        exit(1)
