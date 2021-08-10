ea4b90ebfeaa8166a8a534242b6d787e5a76ae3f
Pranav Karawale <pdkarawale31@gmail.com>
2021-08-10 15:21:15 +0530

    fix: improve number extracting from names

--- run-all.py
+++ run-all.py
@@ -1,18 +1,21 @@
 import os
-import re
 from runner import run
 
-# Regular expression to extract numbers from the filename.
-# Example : program10.py -> 10
-numbers_regex = re.compile(r"([0-9]+)")
-filenums = map(lambda filename: numbers_regex.search(filename) # type: ignore
-               [0], os.listdir("programs"))
+filenums = [
+    # Get the filename (everything before the extension),
+    # and then get the number part of the filename.
+    int(f.split(".")[0].split("program")[1])
+    # For all files in the "program" directory
+    for f in os.listdir("programs")
+    # If the file is a .py file.
+    if f.endswith(".py")
+]
 
 for prog_num in filenums:
     print(f"Running program {prog_num}...")
 
     proc_stderr = run(int(prog_num))
-    
+
     if proc_stderr:
         print("Output of STDERR: ")
         print(proc_stderr)
